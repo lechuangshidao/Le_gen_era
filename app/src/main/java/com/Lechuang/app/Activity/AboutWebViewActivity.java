8d527c7754c65028f794f1164880f80220869693
HouZengYuHou <2393325422@qq.com>
2017-07-27 13:59:26 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	mylibrary/build.gradledi

--- app/src/main/java/com/Lechuang/app/Activity/AboutWebViewActivity.java
+++ app/src/main/java/com/Lechuang/app/Activity/AboutWebViewActivity.java
@@ -37,6 +37,7 @@ public class AboutWebViewActivity extends SimpleTopbarActivity {
         setContentView(R.layout.activity_webview);
         webview = (WebView) findViewById(R.id.webview);
         ToastUtil.showToast("fasfa");
+        ToastUtil.showToast("fasfa");
         Intent intent = getIntent();
         resetTopbarTitle(intent.getStringExtra("title"));
         String type = intent.getStringExtra("type");
