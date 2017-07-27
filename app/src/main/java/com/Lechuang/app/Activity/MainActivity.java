3e7f139ef25418f7b8d12bdf5a0dffdeef506d46
你çandroid_xcd <xcd158454996@163.com>
2017-07-27 14:59:04 +0800

    账号密码临时数据

--- app/src/main/java/com/Lechuang/app/Activity/MainActivity.java
+++ app/src/main/java/com/Lechuang/app/Activity/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends SimpleTopbarActivity {
                 login(SHARE_MEDIA.WEIXIN);
                 break;
             case R.id.text_next:
-                editCallphoneMain.setText("15010481234");
+//                editCallphoneMain.setText("15010481234");
                 String userphone = editCallphoneMain.getText().toString().trim();
                 if (TextUtils.isEmpty(userphone)) {
                     Toast.makeText(MainActivity.this, "请输入手机号", Toast.LENGTH_SHORT).show();
